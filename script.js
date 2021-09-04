c3fde21458fe774f6051c00badc14ffd5a2d7fc8
Peter Armstrong <peterarmstrong999@gmail.com>
2021-09-04 15:04:44 +0100

    end of class update

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 //You can edit ALL of the code here
+const rootElem = document.getElementById("root");
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach(episode => {
+    console.log(episode.name, episode.season, episode.number, episode.image.medium, episode.summary);
+    
+  let episodeCard = document.createElement("span")
+  episodeCard.innerText = episode.name;
+  rootElem.appendChild(episodeCard)
+
+  let episodeImage = src.
+  episodeImage = episode.image.medium
+  rootElem.appendChild(episodeImage)
+
+  })
+
+
+
+
 }
 
 window.onload = setup;
