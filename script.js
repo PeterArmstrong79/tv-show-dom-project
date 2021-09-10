c881126d62ff1d7b57dcbe5b7d2bcc8ee6549ae7
Peter Armstrong <peterarmstrong999@gmail.com>
2021-09-10 11:18:08 +0100

    lvl 100 complete

--- script.js
+++ script.js
@@ -16,7 +16,12 @@ function createCard(episode){
 
   let cardTitle = document.createElement("h2");
   cardTitle.innerText = episode.name;
+  cardTitle.className = "titleAlign"
   card.appendChild(cardTitle);
+
+  let episodeNumber =  document.createElement("h4");
+  episodeNumber.innerText = `S${episode.season.toString().padStart(2, "0")} E${episode.number.toString().padStart(2, "0")}`;
+  card.appendChild(episodeNumber);
 
   let cardImage = document.createElement("img");
   cardImage.src = episode.image.medium;
